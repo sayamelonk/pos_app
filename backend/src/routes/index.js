859c06f55eb9ad3bc5872649dd9dfcd6ee8fba78
ikylonk <dayatketangga95@gmail.com>
2025-01-22 20:41:09 +0700

    Membuat modul category

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import userRouter from "./user.route.js";
 import categoryRoute from "./category.route.js";
+import supplierRouter from "./supplier.route.js";
 
 const router = Router();
 
 router.use("/api", userRouter);
 router.use("/api", categoryRoute);
+router.use("/api", supplierRouter);
 router.use("*", (req, res) => {
   return res.status(404).json({
     message: "Not Found",
