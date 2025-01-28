8e34cc8d72e5c6bd3a792e72b8f222b1901f8a85
ikylonk <dayatketangga95@gmail.com>
2025-01-28 07:40:00 +0700

    Melengkapi kebutuhan dasar frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css'
+import 'react-confirm-alert/src/react-confirm-alert.css'
+import 'react-datepicker/dist/react-datepicker.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
